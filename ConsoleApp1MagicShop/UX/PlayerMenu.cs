a275fda40b8bb6ccb9b55b7eff037f0a5579b33f
BitQu3st <manuel.lusenti92@gmail.com>
2026-04-29 18:49:14 +0200

    Aggiunto menu interattivo e gestione azioni utente
    Implementato ciclo principale con menu in RunAsync, metodi PrintMenu, ChooseActionAsync e GetGoldAction. Inseriti TODO per funzionalità future come inventario, negozio, acquisti, avanzamento giorno e salvataggio.

--- ConsoleApp1MagicShop/UX/PlayerMenu.cs
+++ ConsoleApp1MagicShop/UX/PlayerMenu.cs
@@ -34,11 +34,84 @@ namespace MagicShop.UX
         {
             Console.WriteLine($"Benvenuto {CurrentUser.Username}," +
                 $" hai {CurrentUser.Gold} gold.\n");
+            bool isContinue = true;
+            while (isContinue)
+            {
+                int menuChoose = 0;
+                do
+                {
+                    Console.Clear();
+                    PrintMenu();
+                    Console.Write("Scegli: ");
+                    int.TryParse(Console.ReadLine(), out menuChoose);
+                } while (menuChoose < 1 || menuChoose > 6);
+
+                Console.Clear();
+                isContinue = await ChooseActionAsync(menuChoose);
+            }
         }
 
         //Metodo che stampa semplicemente il menu delle azioni disponibili dal utente
         private void PrintMenu()
         {
+            Console.WriteLine($"---Azioni di {CurrentUser.Username}---\n");
+            Console.WriteLine("1 - Visualizza gold");
+            Console.WriteLine("2 - Visualizza inventario");
+            Console.WriteLine("3 - Tuo negozio");
+            Console.WriteLine("4 - Acquista oggetto");
+            Console.WriteLine("5 - Giorno seguente");
+            Console.WriteLine("6 - Salva e esci");
+        }
+
+        //Metodo che orchestra tutte le interazioni del personaggio avviando
+        //le varie azioni disponibili.
+        //restituisce True se sceglie un azione
+        //restituisce False quando giocatore decide di salvare e uscire per poter gestire logica do while
+        //del ciclo di gioco
+        private async Task<bool> ChooseActionAsync(int choose)
+        {
+            Console.Clear();
+
+            bool IsContinue = true;
+
+            switch (choose)
+            {
+                case 1:
+                    GetGoldAction();
+                    break;
+
+                case 2:
+                    //TODO: DA FARE
+                    break;
+
+                case 3:
+                    //TODO: DA FARE
+                    break;
+
+                case 4:
+                    //TODO: DA FARE
+                    break;
+
+                case 5:
+                    //TODO: DA FARE
+                    break;
+
+                case 6:
+                    ////TODO: DA FARE (implementare salvataggio)
+                    IsContinue = false;
+                    break;
+            }
+
+            return IsContinue;
+        }
+
+        //Metodo che semplicemente pulisce lo schermo e stampa i gold posseduti
+        //dal utente corrente
+        private void GetGoldAction()
+        {
+            Console.WriteLine($"Attualmente hai: {CurrentUser.Gold} gold\n");
+            Console.Write("Premi un tasto per continuare...");
+            Console.ReadKey(true);
         }
     }
 }
